bb6b767177dddfd3116bbcc1961211b72352989d
Tyler M. Roderick <tyler@cloudspot.io>
2021-09-08 20:21:38 -0700

    feat: Update storage on checkbox changes

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 /**
  * Generate options and inject into popup
- * @param {object}
+ * @param {object} config
  */
 const injectHTML = (config) => {
   const app = document.getElementById('app');
@@ -17,7 +17,25 @@ const injectHTML = (config) => {
   app.innerHTML = html;
 };
 
+/**
+ * Listen for checkbox changes from popup and update storage
+ * @param {object} config
+ */
+const setupCheckboxListeners = (config) => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (evt) => {
+      console.log('change');
+      // update storage with new value
+      config[evt.target.id].enabled = evt.target.checked;
+      chrome.storage.sync.set({ config: config });
+    });
+  });
+};
+
 // Initialize the popup with the user's saved settings
 chrome.storage.sync.get('config', ({ config }) => {
   injectHTML(config);
+  setupCheckboxListeners(config);
 });
