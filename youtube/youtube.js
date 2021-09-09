d4daca05bb73756648caed2dcb805c14902e49e5
Tyler M. Roderick <tyler@cloudspot.io>
2021-09-08 20:51:27 -0700

    feat: Implement disable autoplay functionality

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -1,11 +1,35 @@
 ;(() => {
   'use strict';
 
-  /* Selectors */
-  const html = document.querySelector('html');
+  /**
+   * Determine if the user is on a video page by checking the url pathname
+   * @returns {boolean}
+   */
+   const isVideoPage = () => {
+    const { pathname = '' } = window.location || {};
+
+    return pathname === '/watch';
+  };
+
+  /** Toggle the autoplay button to "off" while visiting a video page */
+  const disableAutoPlay = () => {
+    const disableAutoPlayInterval = window.setInterval((window) => {
+      const autoPlayButton = document.querySelector('[data-tooltip-target-id="ytp-autonav-toggle-button"]');
+      const { title = '' } = autoPlayButton || {};
+      const isTurnedOn = title.toLowerCase().includes('on');
+
+      if (isTurnedOn) {
+        autoPlayButton.click();
+      } else {
+        window.clearInterval(disableAutoPlayInterval);
+      }
+    }, 500, window);
+  };
 
   /* Set the HTML element's attibutes based on saved config */
   const toggleAttributes = () => {
+    const html = document.querySelector('html');
+
     chrome.storage.sync.get('config', ({ config }) => {
       for (const key in config) {
         if (config[key].enabled) {
@@ -17,9 +41,22 @@
     });
   };
 
+  /** Disable autoplay on page updates */
+  const handlePageUpdate = () => {
+    chrome.storage.sync.get('config', ({ config }) => {
+      if (isVideoPage() && config.disable_autoplay.enabled) {
+        disableAutoPlay();
+      };
+    });
+  };
+
   /* Initialize all functionality */
   const init = () => {
     toggleAttributes();
+    handlePageUpdate();
+
+    // Listen for youtube navigation changes
+    window.addEventListener('yt-navigate-finish', handlePageUpdate);
   };
 
   init();
