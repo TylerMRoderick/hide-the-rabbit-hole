5b88ad393abb5ea3a939e0a8ada8e436150c12bd
Tyler M. Roderick <tyler@cloudspot.io>
2021-09-08 20:42:13 -0700

    feat: Trigger content script on checkbox toggle

--- popup/popup.js
+++ popup/popup.js
@@ -1,41 +1,57 @@
-/**
- * Generate options and inject into popup
- * @param {object} config
- */
-const injectHTML = (config) => {
-  const app = document.getElementById('app');
-  let html = '';
+;(() => {
+  'use strict';
 
-  for (const key in config) {
-    const val = config[key];
-    html += `<label for="${key}">
-                <input type="checkbox" id="${key}" name="${key}" ${val.enabled ? 'checked' : ''}>
-                ${val.displayName}
-              </label>`;
-  }
+  /**
+   * Generate options and inject into popup
+   * @param {object} config
+   */
+  const injectHTML = (config) => {
+    const app = document.getElementById('app');
+    let html = '';
 
-  app.innerHTML = html;
-};
+    for (const key in config) {
+      const val = config[key];
+      html += `<label for="${key}">
+                  <input type="checkbox" id="${key}" name="${key}" ${val.enabled ? 'checked' : ''}>
+                  ${val.displayName}
+                </label>`;
+    }
 
-/**
- * Listen for checkbox changes from popup and update storage
- * @param {object} config
- */
-const setupCheckboxListeners = (config) => {
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    app.innerHTML = html;
+  };
 
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener('change', (evt) => {
-      console.log('change');
-      // update storage with new value
-      config[evt.target.id].enabled = evt.target.checked;
-      chrome.storage.sync.set({ config: config });
+  /**
+   * Listen for checkbox changes from popup and update storage
+   * @param {object} config
+   */
+  const setupCheckboxListeners = (config) => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('change', (evt) => {
+        // update storage with new value
+        config[evt.target.id].enabled = evt.target.checked;
+        chrome.storage.sync.set({ config: config });
+
+        // Re-execute content script with updated storage
+        executeContentScript();
+      });
     });
-  });
-};
+  };
 
-// Initialize the popup with the user's saved settings
-chrome.storage.sync.get('config', ({ config }) => {
-  injectHTML(config);
-  setupCheckboxListeners(config);
-});
+  /* Execute content script */
+  const executeContentScript = async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['youtube/youtube.js']
+    });
+  };
+
+  // Initialize the popup with the user's saved settings
+  chrome.storage.sync.get('config', ({ config }) => {
+    injectHTML(config);
+    setupCheckboxListeners(config);
+  });
+})();
---
+++ youtube/youtube.js
@@ -0,0 +1,10 @@
+;(() => {
+  'use strict';
+
+  /** Initialize all functionality */
+  const init = () => {
+    console.log('hello from content script');
+  };
+
+  init();
+})();
